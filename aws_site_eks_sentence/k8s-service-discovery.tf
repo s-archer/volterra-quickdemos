99590d65f07a00d793c444e4ededc478acc979b4
Stephen Archer <s.archer@f5.com>
2023-03-02 00:09:35 +0000

    fixed bug. Replaced role_binding with correct cluster_role_binding resource

--- aws_site_eks_sentence/k8s-service-discovery.tf
+++ aws_site_eks_sentence/k8s-service-discovery.tf
@@ -7,6 +7,7 @@ resource "kubernetes_service_account_v1" "f5xc-xxx" {
     name      = "f5xc-xxx"
     namespace = "kube-system"
   }
+  automount_service_account_token =  false
 }
 
 resource "kubernetes_secret_v1" "f5xc-secret-xxx" {
@@ -39,10 +40,9 @@ resource "kubernetes_cluster_role_v1" "f5xc-service-discovery-xxx" {
   }
 }
 
-resource "kubernetes_role_binding_v1" "f5xc-service-discovery-xxx" {
+resource "kubernetes_cluster_role_binding" "f5xc-service-discovery-xxx" {
   metadata {
     name      = "f5xc-service-discovery-xxx"
-    namespace = "kube-system"
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
@@ -65,4 +65,44 @@ resource "local_file" "rendered_kubeconfig" {
     token        = data.kubernetes_secret_v1.f5xc-secret-xxx.data.token
   })
   filename = "${path.module}/rendered_kubeconfig.yaml"
+}
+
+resource "volterra_discovery" "k8s" {
+  name      = "${var.prefix}k8s"
+  namespace = "system"
+
+  discovery_k8s {
+    access_info {
+      // One of the arguments from this list "kubeconfig_url connection_info in_cluster" must be set
+      kubeconfig_url {
+        clear_secret_info {
+          url = format("string:///%s", base64encode(templatefile("${path.module}/k8s-templates/kubeconfig.tpl", {
+            cluster_name = aws_eks_cluster.arch-eks.arn
+            ca_crt       = base64encode("${data.kubernetes_secret_v1.f5xc-secret-xxx.data["ca.crt"]}")
+            server       = aws_eks_cluster.arch-eks.endpoint
+            sa_name      = var.sa_name
+            token        = data.kubernetes_secret_v1.f5xc-secret-xxx.data.token
+          })))
+        }
+      }
+
+      // One of the arguments from this list "isolated reachable" must be set
+      reachable = true
+    }
+
+    publish_info {
+      // One of the arguments from this list "disable publish publish_fqdns dns_delegation" must be set
+      disable = true
+    }
+  }
+  where {
+    site {
+      network_type = "VIRTUAL_NETWORK_SITE_LOCAL_INSIDE"
+      ref {
+        name      = volterra_aws_vpc_site.site.name
+        namespace = volterra_aws_vpc_site.site.namespace
+        tenant    = var.volt_tenant
+      }
+    }
+  }
 }
